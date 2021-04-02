89f97b4a95befc6089bba067ae2d724f8e2666e4
RadinTiholov <radin.tiholov@mail.bg>
2021-04-02 19:06:47 +0300

    Solved the 1st and the 3rd problem of Exam prep4

---
+++ Exam prep4/Exam prep4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Exam_prep4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string input = Console.ReadLine();
+            while (input != "Done")
+            {
+                var splitted = input.Split(" ");
+                if (input.Contains("TakeOdd"))
+                {
+                    StringBuilder newText = new StringBuilder();
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            newText.Append(text[i]);
+                        }
+                    }
+                    text = newText.ToString();
+                    Console.WriteLine(text);
+                }
+                else if (input.Contains("Cut"))
+                {
+                    int index = int.Parse(splitted[1]);
+                    int lenght = int.Parse(splitted[2]);
+                    text = text.Remove(index, lenght);
+                    Console.WriteLine(text);
+
+                }
+                else if (input.Contains("Substitute"))
+                {
+                    string substring = splitted[1];
+                    string substitute = splitted[2];
+                    if (text.Contains(substring))
+                    {
+                        while (text.Contains(substring))
+                        {
+                            text = text.Replace(substring, substitute);
+                        }
+                        Console.WriteLine(text);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {text}");
+        }
+    }
+}
---
+++ Exam prep4/Problem 3. Heroes of Code and Logic VII/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Problem_3._Heroes_of_Code_and_Logic_VII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Hero> dict = new Dictionary<string, Hero>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var hero = Console.ReadLine().Split();
+                Hero help = new Hero()
+                {
+                    HP = int.Parse(hero[1]),
+                    MP = int.Parse(hero[2])
+                };
+                dict.Add(hero[0], help);
+            }
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                var splitted = input.Split(" - ");
+                string command = splitted[0];
+                string name = splitted[1];
+                if (command == "Heal")
+                {
+                    int hp = int.Parse(splitted[2]);
+                    int help = dict[name].HP;
+                    dict[name].HP += hp;
+                    if (dict[name].HP > 100)
+                    {
+                        dict[name].HP = 100;
+                        Console.WriteLine($"{name} healed for {100-help} HP!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} healed for {hp} HP!");
+                    }
+                }
+                else if (command == "Recharge")
+                {
+                    int mp = int.Parse(splitted[2]);
+                    int help = dict[name].MP;
+                    dict[name].MP += mp;
+                    if (dict[name].MP > 200)
+                    {
+                        dict[name].MP = 200;
+                        Console.WriteLine($"{name} recharged for {200 - help} MP!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} recharged for {mp} MP!");
+                    }
+                    
+                }
+                else if (command == "TakeDamage")
+                {
+                    int hp = int.Parse(splitted[2]);
+                    string attacker = splitted[3];
+                    dict[name].HP -= hp;
+                    if (dict[name].HP > 0)
+                    {
+                        Console.WriteLine($"{name} was hit for {hp} HP by {attacker} and now has {dict[name].HP} HP left!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} has been killed by {attacker}!");
+                        dict.Remove(name);
+                    }
+                }
+                else if (command == "CastSpell")
+                {
+                    int mp = int.Parse(splitted[2]);
+                    string spell = splitted[3];
+                    if (mp <= dict[name].MP)
+                    {
+                        dict[name].MP -= mp;
+                        Console.WriteLine($"{name} has successfully cast {spell} and now has {dict[name].MP} MP!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} does not have enough MP to cast {spell}!");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            var sortted = dict.OrderByDescending(x => x.Value.HP).ThenBy(x => x.Key);
+            foreach (var item in sortted)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine($" HP: {item.Value.HP}");
+                Console.WriteLine($" MP: {item.Value.MP}");
+            }
+        }
+    }
+    class Hero
+    {
+        public int HP { get; set; }
+        public int MP { get; set; }
+    }
+}
