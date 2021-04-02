eeedd0ae72c22e4e723522db98a7371d6c595c9d
RadinTiholov <radin.tiholov@mail.bg>
2021-04-02 17:19:23 +0300

    Solved all problems

---
+++ Exam prep3/Problem 2. Destination Mapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Problem_2._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"([\/=])(?<name>[A-Z][A-Za-z]{2,})\1");
+            MatchCollection matches = regex.Matches(input);
+            int sum = 0;
+            List<string> list = new List<string>();
+            foreach (Match item in matches)
+            {
+                sum += item.Groups["name"].Length;
+                list.Add(item.Groups["name"].ToString());
+            }
+            Console.WriteLine($"Destinations: {string.Join(", ", list)}");
+            Console.WriteLine($"Travel Points: {sum}");
+        }
+    }
+}
