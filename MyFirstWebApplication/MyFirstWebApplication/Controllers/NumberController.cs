fec3ff28138a5d714befbcc2159f7de769e2ec49
RadinTiholov <radin.tiholov@mail.bg>
2021-04-04 19:36:18 +0300

    My first web app

---
+++ MyFirstWebApplication/MyFirstWebApplication/Controllers/NumberController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstWebApplication.Controllers
+{
+    public class NumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Generator(int id) 
+        {
+            return View(id);
+        }
+        [HttpPost]
+        public IActionResult NumGenerator(int num)
+        {
+            return View(nameof(Generator), num);
+        }
+    }
+}
---
+++ MyFirstWebApplication/MyFirstWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFirstWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
